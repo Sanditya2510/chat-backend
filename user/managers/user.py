5bfd35062f6fce43b63687dabcc2317a4061d223
sanditya2510 <sanditya2510@gmail.com>
2021-04-28 16:31:19 +0530

    - [Fix] added requirements - [Fix] added username as a function param in user manager

--- user/managers/user.py
+++ user/managers/user.py
@@ -5,7 +5,7 @@ from django.utils.translation import ugettext_lazy as _
 
 class UserManager(BaseUserManager):
     use_in_migrations = True
-    def _create_user(self, email, password, **extra_fields):
+    def _create_user(self, username, email, password, **extra_fields):
         """
         Base function to create and save a User with the given email and password.
         Raises:
@@ -24,7 +24,7 @@ class UserManager(BaseUserManager):
         user.save(using=self._db)
         return user
 
-    def create_user(self, email, password, **extra_fields):
+    def create_user(self, username, email, password, **extra_fields):
         """
         Create and save a regular User with the given email and password.
         """
@@ -33,7 +33,7 @@ class UserManager(BaseUserManager):
         extra_fields.setdefault('is_superuser', False)
         return self._create_user(username, email, password, **extra_fields)
 
-    def create_superuser(self, email, password, **extra_fields):
+    def create_superuser(self, username, email, password, **extra_fields):
         """
         Create and save a SuperUser with the given email and password.
         """
