bb0496417adc237aed9087488ce5ca6c50169734
sanditya2510 <sanditya2510@gmail.com>
2021-05-03 15:05:29 +0530

    - [Feat.] automated creation of superuser in docker

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,7 +11,7 @@ then
     echo "PostgreSQL started"
 fi
 
-python manage.py flush --no-input
 python manage.py migrate
+echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('$SUPERUSER_USERNAME', '$SUPERUSER_EMAIL', '$SUPERUSER_PASSWORD')" | python manage.py shell
 
 exec "$@"
